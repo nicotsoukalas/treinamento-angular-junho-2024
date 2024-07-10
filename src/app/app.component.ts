944bda752aeacd61ee2f57e096302168e5bdbb04
nicotsoukalas <nicotsoukalas@gmail.com>
2024-07-10 20:53:58 -0300

    Estudo de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuItem } from './interfaces/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +7,10 @@ import { MenuItem } from './interfaces/menu-item';
 })
 export class AppComponent {
   title = 'treinamento-angular-junho-2024';
-  
-  items: MenuItem[] = [
-    {
-      'name': 'Home',
-      'url': '/home'
-    },
-    {
-      'name': 'Profiles',
-      'url': '/profile',
-    },
-  ]
 
-  exibirAlerta(has: string) {
-    alert(`O palmeiras ${has} mundial`);
-  }
+exibirAlerta(has: string) {
+  console.log('MINHA FUNCAO')
+  alert(`O Palmeiras ${has} Mundial`);
+}
+
 }
